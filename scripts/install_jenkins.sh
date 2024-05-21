1828b4c63eadc9af110bc86e278dccc88943dea2
AC <ac@marketmining.com.br>
2024-05-20 21:02:35 -0300

    Setup

--- scripts/install_jenkins.sh
+++ scripts/install_jenkins.sh
@@ -9,8 +9,9 @@ sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
+sudo apt update
 sudo apt-get install -y jenkins
+sudo usermod -aG docker jenkins
 
 echo "**** Jenkins Admin Password"
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
--- scripts/provision.sh
+++ scripts/provision.sh
@@ -7,7 +7,3 @@ sh /vagrant/scripts/install_docker.sh
 sudo usermod -aG docker vagrant
 
 sh /vagrant/scripts/install_jenkins.sh
-sudo usermod -aG docker jenkins
-
-
-
